d77be22ba5b78b878392c31d58ecac603e2b0d16
Luis <81855420+luisoos@users.noreply.github.com>
2022-11-03 19:56:34 +0100

    ✨ feat: invite wave fetching

--- src/commands/InviteWave.ts
+++ src/commands/InviteWave.ts
@@ -1,5 +1,10 @@
 import { CommandInteraction, Client } from "discord.js";
+import axios from "axios";
+import dotenv from 'dotenv';
 import { Command } from "../interfaces/Command";
+import GetUsersResponse from "./../types/GetUsersResponse";
+
+dotenv.config();
 
 export const InviteWave: Command = {
     name: "invitewave",
@@ -7,18 +12,40 @@ export const InviteWave: Command = {
     type: 1,
     defaultMemberPermissions: 'ADMINISTRATOR',
     run: async (client: Client, interaction: CommandInteraction) => {
-        const members = interaction.guild!.members.cache.map(member => member.id);
-        members.forEach(member => {
-            /* member = discord id of member
-                get host user id
-                give host user invites
-            */
-        });
-        const content = `Successfully gave an invite to everyone.`;
+        const { status, data } = await axios.get<GetUsersResponse>(
+            `${process.env.baseurl}/api/profile/get`,
+            {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `${process.env.jwt}`
+                },
+            }
+        );
 
-        await interaction.followUp({
-            ephemeral: true,
-            content
-        });
+        if (status === 200) {
+            const content = `Successfully gave an invite to everyone.`;
+
+            await interaction.followUp({
+                ephemeral: true,
+                content
+            });
+            return;
+        } else if (status === 400) {
+            const content = `Invite only is turned off.`;
+
+            await interaction.followUp({
+                ephemeral: true,
+                content
+            });
+            return;
+        } else {
+            const content = `Error while creating invite wave.`;
+
+            await interaction.followUp({
+                ephemeral: true,
+                content
+            });
+            return;
+        }
     }
 }; 
